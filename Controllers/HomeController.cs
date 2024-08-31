8f3c0dd807f69372d18c9c96b2730dcf8e325436
tommy w <tlwjr27@gmail.com>
2024-08-30 19:19:38 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieList.Models;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx) => context = ctx;
+
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m =>m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        //sets db options
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+        //creates movie dbset
+        public DbSet<Movie> Movies { get; set; } = null;
+        public DbSet<Genre> Genres { get; set; } = null;
+
+        
+
+        //method to create data set on creation on the table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genre>().HasData(
+               new Genre { GenreId = "A", Name = "Action" },
+               new Genre { GenreId = "C", Name = "Comedy" },
+               new Genre { GenreId = "D", Name = "Drama" },
+               new Genre { GenreId = "H", Name = "Horro" },
+               new Genre { GenreId = "M", Name = "Muscial" },
+               new Genre { GenreId = "R", Name = "RomCom" },
+               new Genre { GenreId = "S", Name = "SciFi" }
+               );
+
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 1,
+                    Name = "Casablanca",
+                    Year = 1942,
+                    Rating = 5,
+                    GenreId = "D"
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3,
+                    GenreId = "A"
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4,
+                    GenreId = "R"
+                }
+
+                );
+           
+        }
+    }
+}
